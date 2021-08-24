59b45af83f526ddfa2e44df2251f59e2e4fb614e
Anita Nagy <anitanagy1989@gmail.com>
2021-08-24 11:12:49 +0200

    feat: add events to startup configuration

--- Models/EventItem.cs
+++ Models/EventItem.cs
@@ -4,7 +4,7 @@ namespace dotnet_enterprise.Models
 {
     public class EventItem
     {
-        public long EventId { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public bool IsFavorite { get; set; }
         public string Description { get; set; }
