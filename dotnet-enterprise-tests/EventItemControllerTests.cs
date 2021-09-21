764481bb4ad23586548ea83f9ba03d8073ca429f
Karcub <rokaszemumenyecske@gmail.com>
2021-09-21 14:37:37 +0200

    test: GetEventItem_IfItemDoesntExist_ReturnNull

--- dotnet-enterprise-tests/EventItemControllerTests.cs
+++ dotnet-enterprise-tests/EventItemControllerTests.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using dotnet_enterprise.Controllers;
 using dotnet_enterprise.Interfaces;
 using dotnet_enterprise.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Any;
 using NUnit.Framework;
@@ -38,13 +41,11 @@ namespace dotnet_enterprise_tests
         }
         
         [Test]
-        public void GetEventItem_IfItemDoesntExist_ReturnItem()
+        public void GetEventItem_IfItemDoesntExist_ReturnNull()
         {
-            var eventItem = new EventItem { Id = 1 };
-            var eventTask = Task.Run(() => eventItem);
-
-            _mockRepository.GetEventItem(1).Returns(eventTask);
-            Assert.That(_controller.GetEventItem(1).Result.Value, Is.EqualTo(eventItem));
+            // var response = new HttpResponseMessage(HttpStatusCode.NotFound);
+            // Assert.That(_controller.GetEventItem(123), Is.EqualTo(response));
+            Assert.That(_controller.GetEventItem(1).Result.Value, Is.EqualTo(null));
         }
     }
 }
