ba0c5e8585ec36405e4e3771f3ad38348fda5e5d
Karcub <67015556+Karcub@users.noreply.github.com>
2021-09-23 14:15:24 +0200

    chore: create migration for updated data

---
+++ dotnet-enterprise/Migrations/20210923121026_SeedUpdatedDatabase.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_enterprise.Migrations
+{
+    public partial class SeedUpdatedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 26L,
+                column: "Category",
+                value: "THEATER");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 27L,
+                column: "Category",
+                value: "THEATER");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 28L,
+                column: "Category",
+                value: "THEATER");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 29L,
+                column: "Category",
+                value: "THEATER");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 30L,
+                columns: new[] { "Category", "City", "Location", "Subtitle" },
+                values: new object[] { "THEATER", "Budapest", "Budapest Bábszínház, Nagyszínpad <br>1062 Budapest, Andrássy út 69.", "Ajánlott korhatár: +3" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 26L,
+                column: "Category",
+                value: "OPERA");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 27L,
+                column: "Category",
+                value: "OPERA");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 28L,
+                column: "Category",
+                value: "OPERA");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 29L,
+                column: "Category",
+                value: "MUSICAL");
+
+            migrationBuilder.UpdateData(
+                table: "EventItems",
+                keyColumn: "Id",
+                keyValue: 30L,
+                columns: new[] { "Category", "City", "Location", "Subtitle" },
+                values: new object[] { "PUPPET-THEATER", "Budapest Bábszínház, Nagyszínpad <br>1062 Budapest, Andrássy út 69.", "Budapest", "Ajánlott életkor: 3+" });
+        }
+    }
+}
